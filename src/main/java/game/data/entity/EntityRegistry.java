2cdb2ab65c916bec177788b95c0430ba996e69b3
OliviaTheVampire <sindrefagerheim53@gmail.com>
2026-01-07 02:24:33 +0100

    Read player info actions as VarInt

--- src/main/java/game/data/entity/EntityRegistry.java
+++ src/main/java/game/data/entity/EntityRegistry.java
@@ -84,7 +84,7 @@ public class EntityRegistry {
 
     public void updatePlayerAction(DataTypeProvider provider) {
         executor.execute(() -> attempt(() -> {
-            byte actions = provider.readNext();
+            int actions = provider.readVarInt();
             int playerCnt = provider.readVarInt();
 
             for (int i = 0; i < playerCnt; i++) {
